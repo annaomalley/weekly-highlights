9f7bf79a3122e3100cd112f834b01f713addb6b6
Kaustdeo <kaustubhdeo@msn.com>
2016-09-10 04:24:49 -0400

    Saves Selection to storage

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ document.addEventListener('mouseup',function(event)
 {
 	var sel = window.getSelection().toString();
 
-    if(sel.length)
+    if(sel.length) {
+        chrome.storage.sync.set({'value': sel}, function() {
+        	console.log('saved');
+        });
+		chrome.storage.sync.get('value', function(sel2) {
+			console.log(sel2.value);
+		});
+        
         //chrome.extension.sendRequest({'message':'setText','data': sel},function(response){})
+    }
 
 })
 
