644ca2b81c0ee541f3979a5e5649a07d3bfaa6a4
Kaustdeo <kaustubhdeo@msn.com>
2016-09-10 19:42:12 -0400

    added Title and URL

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-var highlightArray = [];
-var count = 0;
-
  window.onkeypress = function(event) {
  	if (event.keyCode == 115 || event.keyCode == 83) {
 
 
 		var sel = window.getSelection().toString();
-		
+
 		if(sel.length) {
+			var url = window.location.href;
+			var title = window.document.title;
+			console.log(url); 
 			chrome.storage.sync.get({storedLines: []}, function(result) {
 				var storedLines = result.storedLines;
 
-				storedLines.push({selection: sel});
+				storedLines.push({selection: sel, url, title});
 	        	chrome.storage.sync.set({storedLines: storedLines}, function () {
         			// you can use strings instead of objects
         			// if you don't  want to define default values
-        			// chrome.storage.sync.get('storedLines', function (result) {
-           //  			console.log(result.storedLines)
-        			// });
+        			chrome.storage.sync.get('storedLines', function (result) {
+           			console.log(result.storedLines)
+        			});
     			});
 
     		});
