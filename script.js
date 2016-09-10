7f01bc219619ed4e810d11ff26a245e0de9a5b4f
Kaustdeo <kaustubhdeo@msn.com>
2016-09-10 18:23:17 -0400

    puts selection in an arry

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+var highlightArray = [];
+var count = 0
+
 document.addEventListener('mouseup',function(event)
 {
 	var sel = window.getSelection().toString();
-
-    if(sel.length) {
+	
+	if(sel.length) {
         chrome.storage.sync.set({'value': sel}, function() {
-        	console.log('saved');
-        });
-		chrome.storage.sync.get('value', function(sel2) {
-			console.log(sel2.value);
+        	console.log('Saved')
+    	});
+
+
+        chrome.storage.sync.get(['value'], function(result) {
+			highlightArray.unshift(result);
+			count++;
+			var newLine = highlightArray[0];
+			window.console.log(newLine);
+			
+			for (i = 0; i < count; i++) {
+				window.console.log(highlightArray[i]);	
+			}
+			
 		});
         
         //chrome.extension.sendRequest({'message':'setText','data': sel},function(response){})
